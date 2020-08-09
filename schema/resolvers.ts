3d8a8b57adf0de806f2a66b5b803576f08922561
mojo706 <xriseugene@gmail.com>
2020-08-09 18:03:33 +0300

    add simple graphql implementation

---
+++ schema/resolvers.ts
@@ -0,0 +1,16 @@
+import { DateTimeResolver, URLResolver } from 'graphql-scalars';
+import { chats } from '../db';
+ 
+const resolvers = {
+  Date: DateTimeResolver,
+  URL: URLResolver,
+ 
+  Query: {
+    chats() {
+      return chats;
+    },
+  },
+};
+ 
+export default resolvers;
+
